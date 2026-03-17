510061ff05a113ee6b2c75b9f21a312d5245bbcd
Andrew Erickson <aerickson@mozilla.com>
2026-03-17 01:24:14 -0700

    add helper script for finding bad linux hosts

---
+++ tools/list_bad_linux_hosts.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# lists hosts that havne't talked to tc and have been unreachable for awhile
+#
+# idea: use this as a source of hosts to reset
+
+filter="tc_act>4h data>4h note=-"
+# filter="tc_act>4h note=-"
+
+uv run fleetroll host-monitor  \
+    configs/host-lists/linux/all.list \
+    --once \
+    --hostname-only \
+    --filter "$filter"
